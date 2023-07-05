187abae3e2006d289f02cae9e8f325ea286e8d09
yash-r-gaikwad <138587600+yash-r-gaikwad@users.noreply.github.com>
2023-07-05 15:57:46 +0530

    Router changed to Hashrouter since github pages don't support Browser Router

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -7,11 +7,11 @@ import Pricing from './pages/Pricing';
 
 
 import {
-  createBrowserRouter,
+  createHashRouter,
   RouterProvider,
 } from "react-router-dom";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <Homepage />,
