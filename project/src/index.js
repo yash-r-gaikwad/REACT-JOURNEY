47cad0cfd135432a8a109d2677130efedd88a4a2
yash-r-gaikwad <138587600+yash-r-gaikwad@users.noreply.github.com>
2023-07-05 16:06:15 +0530

    Index changes

--- project/src/index.js
+++ project/src/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
+import { createHashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     <HashRouter>
+     <createHashRouter>
     <App />
-    </HashRouter>
+    </createHashRouter>
+    
   </React.StrictMode>
 );
 
